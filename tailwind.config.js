79fd1712105bd7b72a79936d16555de5dba74b4c
sami <sakuma.natsu3@gmail.com>
2024-02-19 22:26:47 +0900

    tailwindとdaisyuiの環境構築

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: [
+      "bumblebee"
+    ],
+  },
 }
