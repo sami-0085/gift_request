d24d8c52c99517bb91bc718150efe2ccfcc12ab3
sami <sakuma.natsu3@gmail.com>
2024-02-24 13:52:20 +0900

    edit userstable

---
+++ db/migrate/20240224033403_sorcery_core.rb
@@ -0,0 +1,12 @@
+class SorceryCore < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email,            null: false, index: { unique: true }
+      t.string :crypted_password
+      t.string :salt
+      t.string "name",            null: false
+
+      t.timestamps                null: false
+    end
+  end
+end
