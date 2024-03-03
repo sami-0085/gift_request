fe5fc2154938110b8d3194ecd5e90a193564ddef
sami <sakuma.natsu3@gmail.com>
2024-03-03 17:45:52 +0900

    add 画像のバリデーション機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 255 }
+  validates :avatar, content_type: { in: %w[image/jpeg image/gif image/png],
+                    message: "有効なフォーマットではありません" },
+                    size: { less_than: 5.megabytes, message: " 5MBを超える画像はアップロードできません" }
   # validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
 end
