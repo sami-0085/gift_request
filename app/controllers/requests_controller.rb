aa30d490ac80af67c0022d29579b5a72166a6935
sami <sakuma.natsu3@gmail.com>
2024-03-06 18:00:43 +0900

    add プロンプト作成

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,11 +4,33 @@ class RequestsController < ApplicationController
 
   def search
     @client = OpenAI::Client.new(access_token: @api_key)
+    # response = @client.chat(
+    #   parameters: {
+    #       model: "gpt-3.5-turbo",
+    #       messages: [{ role: "user", content: @query }],
+    #   })
     response = @client.chat(
       parameters: {
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: @query }],
-      })
+          model: "gpt-4",
+          messages: [
+            { role: "system", content: "あなたは作詞家です" },
+            { role: "user", content:
+              "以下の条件で歌詞を出題してください。
+
+              # 条件
+              テーマ: #{@query}
+              歌詞にテーマの言葉#{@query}は使わないでください。
+              テイスト: ラブソング
+              Output should be less than 300 tokens
+
+              # output
+              タイトル: 20文字以内
+              歌詞: 句読点で改行して下さい。
+              テーマのヒント:30文字以内で3つ、format:-ヒント1
+              テーマ: 3択、format:-選択肢1"}
+          ],
+      }
+    )
 
     @chats = response.dig("choices", 0, "message", "content")
   end
