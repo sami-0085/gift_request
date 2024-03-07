76398316bf0019964e1f10f047c4c2c7ceb703fb
sami <sakuma.natsu3@gmail.com>
2024-03-08 01:24:26 +0900

    add request-show,choiceモデル

---
+++ db/migrate/20240307162202_create_choices.rb
@@ -0,0 +1,11 @@
+class CreateChoices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :choices do |t|
+      t.references :request, null: false, foreign_key: true
+      t.integer :number
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
