dc807c7793e35006d98750e0acb61b1e7949e646
sami <sakuma.natsu3@gmail.com>
2024-03-10 09:28:12 +0900

    add gem_rails-i18n

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module App
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
 
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
