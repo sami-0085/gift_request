c612f80bb819e7c37f39fa4191e4937fd0ed2c87
sami <sakuma.natsu3@gmail.com>
2024-03-19 12:54:49 +0900

    update クエストがボタン押すと表示される

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,18 +1,10 @@
 class QuestsController < ApplicationController
-  skip_before_action :require_login, only: %i[show choice show_card]
-  before_action :set_quest, only: %i[show choice show_card]
+  skip_before_action :require_login, only: %i[show choice]
+  before_action :set_quest, only: %i[show choice]
 
   def show; end
 
   def choice; end
-
-  def show_card
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to choice_quest_path(@quest), alert: "Action not supported." }
-    end
-  end
-
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
     member do
       get :choice
       post :answer
-      get :show_card
     end
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
