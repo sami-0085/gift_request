faf135e056e72684c8addf68af584af56850c21c
sami <sakuma.natsu3@gmail.com>
2024-03-22 01:17:55 +0900

    update SVGの色修正、クエスト文校正、ヘッダードロップダウンz-index修正

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -7,56 +7,26 @@ class QuestsController < ApplicationController
   def choice; end
 
   def answer
-# choice ビューからの入力を受け取る
-user_input = params[:name]&.strip
-
-# hint ビューからの選択を受け取る
-selected_choice = params[:selected_choice]&.strip
-
-# ユーザー入力が存在するか、選択された選択肢が存在するかを確認
-answer = user_input.presence || selected_choice
-
-# 正解を判定する条件（ここでは例として @quest オブジェクトから正解を取得する）
-correct_answer = @quest.name.strip
-
-if answer.present? && correct_answer == answer
-  # 正解の場合の処理
-  redirect_to correct_quest_path(@quest), notice: "正解です！おめでとうございます！"
-else
-  # 不正解または入力がない場合の処理
-  redirect_to hint_quest_path(@quest), alert: "残念、不正解です。手がかりをヒントにもう一度挑戦しよう！"
-end
-
-
-
-    # user_input = params[:name]
-    # quest_name = @quest.name.strip
-
-    # if user_input.present? && quest_name.include?(user_input)
-    #   # 選択した選択肢が正解の場合、正解ページにリダイレクト
-    #   redirect_to correct_quest_path(@quest), notice: "正解です！ラストストーリーを読もう！"
-    # else
-    #   # 不正解の場合、選択肢のページにリダイレクト
-    #   redirect_to hint_quest_path(@quest), alert: "残念。もう一度挑戦してください。"
-    # end
+    # choice ビューからの入力を受け取る
+    user_input = params[:name]&.strip
+    # hint ビューからの選択を受け取る
+    selected_choice = params[:selected_choice]&.strip
+    # ユーザー入力が存在するか、選択された選択肢が存在するかを確認
+    answer = user_input.presence || selected_choice
+    # 正解を判定する条件（ここでは例として @quest オブジェクトから正解を取得する）
+    correct_answer = @quest.name.strip
+    if answer.present? && correct_answer == answer
+      # 正解の場合の処理
+      redirect_to correct_quest_path(@quest), notice: "正解です！おめでとうございます！"
+    else
+      # 不正解または入力がない場合の処理
+      redirect_to hint_quest_path(@quest), alert: "残念、不正解です。手がかりをヒントにもう一度挑戦しよう！"
+    end
   end
 
-  def correct
+  def correct; end
 
-  end
-
-  def hint
-    # selected_choice = params[:selected_choice]&.strip
-    # quest_name = @quest.name.strip
-
-    # if selected_choice.present? && quest_name.include?(selected_choice)
-    #   # 選択した選択肢が正解の場合、正解ページにリダイレクト
-    #   redirect_to correct_quest_path(@quest), notice: "正解です！ラストストーリーを読もう！"
-    # else
-    #   # 不正解の場合、選択肢のページにリダイレクト
-    #   redirect_to hint_quest_path(@quest), alert: "残念。もう一度挑戦してください。"
-    # end
-  end
+  def hint; end
 
   private
 
