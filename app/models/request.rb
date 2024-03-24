a19815eff11baf5012426c751b27aec6e3305a53
sami <sakuma.natsu3@gmail.com>
2024-03-25 02:47:29 +0900

    update リクエスト詳細表示スクロール導入前

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,12 @@ class Request < ApplicationRecord
   has_many :hints, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
+
+  def hint_by_number(number)
+    hints.find_by(number: number).content
+  end
+
+  def choice_by_number(number)
+    choices.find_by(number: number).content
+  end
 end
